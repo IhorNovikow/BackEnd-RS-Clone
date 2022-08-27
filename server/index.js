ee949939a3e1550dc6ff731b97c80ed054b85c6c
Jafr0 <zhek42@yandex.ru>
2022-08-27 15:47:19 +0300

    update: server on port 3000

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const authRouter = require('./authRouter');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = express();
+
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://Jafr0:Jafr0228@cluster0.i7uke4d.mongodb.net/mono-rs-clone', {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        app.listen(PORT, () => {
+            console.log('Server run on Port 3000');
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
