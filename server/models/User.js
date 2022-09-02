af428c2d0bd9b0695a981247427b7e99fcdf9205
Jafr0 <zhek42@yandex.ru>
2022-09-02 21:24:52 +0300

    update: add users score

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const User = new Schema({
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    roles: [{ type: String, ref: 'Role' }],
+	username: { type: String, unique: true, required: true },
+	password: { type: String, required: true },
+	roles: [{ type: String, ref: 'Role' }],
+	score: { type: String },
 });
 
 module.exports = model('User', User);
