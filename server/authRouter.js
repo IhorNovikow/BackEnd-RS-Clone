f9f6ebe0c81cacb332cca447f305bc9233ed1f7e
Jafr0 <zhek42@yandex.ru>
2022-09-04 15:46:20 +0300

    update: score for users

--- server/authRouter.js
+++ server/authRouter.js
@@ -2,16 +2,42 @@ const Router = require('express');
 const router = new Router();
 const controller = require('./authController');
 const { check } = require('express-validator');
+const User = require('./models/User');
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+app.use(bodyParser.json())
+app.use(express.json())
+
 
 router.post(
-    '/registration',
-    [
-        check('username', 'Имя пользователья не может быть пустым').notEmpty(),
-        check('password', 'Пароль должен быть от 4 до 10 символов').isLength({ min: 4, max: 10 }),
-    ],
-    controller.registration
+	'/registration',
+	[
+		check('username', 'Имя пользователья не может быть пустым').notEmpty(),
+		check('password', 'Пароль должен быть от 4 до 10 символов').isLength({ min: 4, max: 10 }),
+	],
+	controller.registration
 );
 router.post('/login', controller.login);
 router.get('/users', controller.getUsers);
+
+router.put(`/users/:username/`, async function getUser(req, res) {
+
+	try {
+
+		const user = await User.updateOne(
+			{ "username": req.params.username }, // Filter
+			{ $set: { 'score': req.body.score } }, // Update
+
+
+		)
+
+		res.json(user);
+
+	} catch (e) {
+		console.log(e)
+	}
+});
+
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -3,35 +3,37 @@ const mongoose = require('mongoose');
 
 const authRouter = require('./authRouter');
 const cors = require('cors');
-
+const bodyParser = require('body-parser')
 const PORT = 3000;
 
 const app = express();
 
 const corsOptions = {
-    origin: '*',
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
+	origin: '*',
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
+app.use(bodyParser.json())
 app.use('/auth', authRouter);
+app.patch('auth/users/:username')
 
 async function start() {
-    try {
-        await mongoose.connect('mongodb+srv://Jafr0:Jafr0228@cluster0.i7uke4d.mongodb.net/mono-rs-clone', {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
-        app.listen(PORT, () => {
-            console.log('Server run on Port 3000');
-        });
-    } catch (e) {
-        console.log(e);
-    }
+	try {
+		await mongoose.connect('mongodb+srv://Jafr0:Jafr0228@cluster0.i7uke4d.mongodb.net/mono-rs-clone', {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		app.listen(PORT, () => {
+			console.log('Server run on Port 3000');
+		});
+	} catch (e) {
+		console.log(e);
+	}
 }
 
 start();
